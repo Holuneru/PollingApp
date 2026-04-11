a5a7df88b033fe585d507a0c449bd0d737b0844b
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-04-11 13:19:08 +0300

    Add getAllOptionsByPollId DONE

--- src/main/java/com/example/pollingapp/Contollers/OptionController.java
+++ src/main/java/com/example/pollingapp/Contollers/OptionController.java
@@ -1,15 +1,15 @@
 package com.example.pollingapp.Contollers;
 
 import com.example.pollingapp.DTO.OptionDto.Request.OptionRequestDto;
+import com.example.pollingapp.DTO.OptionDto.Response.OptionResponse;
 import com.example.pollingapp.Service.OptionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/options")
@@ -21,6 +21,11 @@ public class OptionController {
     public ResponseEntity<String> addOption(@RequestBody @Valid OptionRequestDto optionRequestDto){
         optionService.addOption(optionRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Option added successfully");
+    }
+
+    @GetMapping(path = "/poll/{pollId}")
+    public List<OptionResponse> getAllOptionsByPoll(@PathVariable Long pollId){
+        return optionService.getAllOptionByPoll(pollId);
     }
 
 }
